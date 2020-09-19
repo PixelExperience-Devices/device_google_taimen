1edcc9de48b095850f86f672b9b385dcfbbd06a5
Lunarixus <Nathan@lunarixus.party>
2020-09-19 13:59:37 +0000

    taimen: Set TARGET_SCREEN_DENSITY
    For some reason Android 11 ignores the ro.sf.lcd_density prop, so set the flag instead.
    
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- device.mk
+++ device.mk
@@ -42,9 +42,6 @@ PRODUCT_COPY_FILES += \
     device/google/taimen/init-taimen.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init-$(PRODUCT_HARDWARE).rc \
     device/google/taimen/init.taimen.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).usb.rc
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.sf.lcd_density=560 \
-
 # Kernel Modules Config
 PRODUCT_COPY_FILES += \
     device/google/taimen/init.insmod.cfg:$(TARGET_COPY_OUT_VENDOR)/etc/init.insmod.cfg \
