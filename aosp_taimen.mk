a485247beceb26ff1e72eab3706ff3d346d095ba
Lunarixus <Nathan@lunarixus.party>
2020-12-07 22:17:13 +0100

    taimen: Don't restrict vendor owner
    Signed-off-by: Lunarixus <Nathan@lunarixus.party>

--- aosp_taimen.mk
+++ aosp_taimen.mk
@@ -43,8 +43,6 @@ $(call inherit-product, vendor/google/taimen/taimen-vendor.mk)
 PRODUCT_COPY_FILES += \
     device/google/taimen/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml
 
-PRODUCT_RESTRICT_VENDOR_FILES := owner
-
 PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
 
 PRODUCT_MANUFACTURER := Google
