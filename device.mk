759f5bc0e39d1ba340bd52ee6cad0c63528ee8cb
PixelBoot <bootingpixels@gmail.com>
2021-11-22 14:07:50 -0800

    Revert "Use on_vendor VNDK APEX for non-REL build"
    While this makes sense on dynamic partition devices where this
    generated a smaller binary, on devices without dynamic partitions
    this uses valuable system space which should be used for gapps
    and instead should be kept on the vendor partition.
    
    This reverts commit 038066f.
    
    Change-Id: Ide5a0a714eb4f4cf65ef72542f287d32242531ad

--- device.mk
+++ device.mk
@@ -20,6 +20,8 @@ PRODUCT_AAPT_PREBUILT_DPI := xxxhdpi xxhdpi xhdpi hdpi
 
 PRODUCT_HARDWARE := taimen
 
+PRODUCT_PACKAGES += com.android.vndk.current.on_vendor
+
 # To build taimen specific modules e.g. librecovery_ui_taimen.
 PRODUCT_SOONG_NAMESPACES += device/google/taimen
 
